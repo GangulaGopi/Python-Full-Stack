2165f58934549156c8822a4b029ab2f6ce9fc7c8
gopi <gopigangula2004@gmail.com>
2026-04-15 09:56:00 +0530

    basics on ds

---
+++ Python/Programming/Data Structures.py
@@ -0,0 +1,10 @@
+l=[1,2,3,4,5,6,7,8,98]
+print(l)
+for i in l:
+    print(i,end=" ")
+for i in range(0,len(l)):
+    print (l[i],end=" ")
+index=0
+while(index<len(l)):
+    print(l[index],end=" ")
+    index+=1
